fd0b3478f2c2c7acb3cce80a42429cafc7ca24b0
juansavero <21839784-juansavero@users.noreply.replit.com>
2023-05-03 11:30:50 +0000

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Icon Tittle -->
+    <link rel="shortcut icon" href="img/title.png" />
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+
+    <!-- Boostrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" />
+
+    <!-- AOS -->
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="style.css" />
+
+    <!-- Jquery 3.6.0 -->
+    <script type="text/javascript" src="jquery-3.6.0.min.js"></script>
+    <script type="text/javascript">
+    	<script type="text/javascript" src="jquery-3.6.0.min.js"></script>
+	      <script type="text/javascript">
+		        $(document).ready(function(){
+			    $("a").dblclick(function(){
+				    alert("Welcome To My Gituhub Acount");
+			      });
+	    	  });
+	     </script>
+    </script>
+
+    <title>My Portofolio | Juan Savero</title>
+
+
+  </head>
+  <body id="home">
+
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">PORTOFOLIO</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#home"><i class="bi bi-house"> HOME</i></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#about"><i class="bi bi-file-earmark-person"> ABOUT</i></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#experience"><i class="bi bi-star"> EXPERIENCE</i></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#message"><i class="bi bi-envelope"> MESSAGE</i></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Akhir navbar -->
+
+    <!-- Jumbotron -->
+    <section class="jumbotron text-center">
+      <img src="img/profile1.jpg" alt="Juan Savero" width="200" class="rounded-circle img-thumbnail" />
+
+      <h1 class="display-4">Juan Savero</h1>
+      <p class="lead">INFORMATION SYSTEM | SRIWIJAYA UNIVERSITY </p>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#ffff"
+          fill-opacity="1"
+          d="M0,96L48,101.3C96,107,192,117,288,138.7C384,160,480,192,576,224C672,256,768,288,864,288C960,288,1056,256,1152,229.3C1248,203,1344,181,1392,170.7L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+        ></path>
+      </svg>
+    </section>
+
+    <!-- Akhir Jumbotron -->
+
+    <!-- About -->
+    <section id="about">
+      <div class="container">
+        <div class="row text-center mb-3">
+          <div class="col">
+            <h2><p class="fw-light">ABOUT ME</p></h2>
+          </div>
+        </div>
+        <div class="row justify-content-center fs-5 text-center">
+          <div class="col-md-4" data-aos="fade-right" data-aos-duration="1000" data-aos-delay="200">
+            <p>Hello! My name is Juan Savero and I majored in Information Systems. My busy life in college is focused on gaining knowledge in the field of technology and on the side besides studying I focus on organization at BEM KM Fasilkom Unsri which has been running for 1.5 years.</p>
+          </div>
+          <div class="col-md-4" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="100">
+            <p>
+              my goal in entering this department is to deepen UI/UX knowledge, explore the front end developer and application science liaison between business and technology department,with the aim of my hope that I can pursue what I want and be firm with the grip of the phrase "who is serious - he gets it".
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#e2edff"
+          fill-opacity="1"
+          d="M0,288L48,282.7C96,277,192,267,288,224C384,181,480,107,576,112C672,117,768,203,864,213.3C960,224,1056,160,1152,149.3C1248,139,1344,181,1392,202.7L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+        ></path>
+      </svg>
+    </section>
+
+    <!-- Akhir about -->
+
+    <!-- Projects -->
+    <section id="experience">
+      <div class="container">
+        <div class="row text-center mb-3">
+          <div class="col">
+            <h2><p class="fw-light">EXPERIENCE</p></h2>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-4 mb-3">
+            <div class="card" data-aos="flip-left" data-aos-delay="100" data-aos-duration="500">
+              <img src="img/project/1.jpg" class="card-img-top" alt="Projects 1" />
+              <div class="card-body">
+                <p class="card-text fw-bold text-center">TECHNOLOGY EUPHORIA 2022</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="card" data-aos="flip-left" data-aos-delay="200" data-aos-duration="500">
+              <img src="img/project/2.jpg" class="card-img-top" alt="Projects 2" />
+              <div class="card-body">
+                <p class="card-text fw-bold text-center">BEM STAFF ADMISSION COMMITTEE</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="card" data-aos="flip-left" data-aos-delay="300" data-aos-duration="500">
+              <img src="img/project/3.jpg" class="card-img-top" alt="Projects 3" />
+              <div class="card-body">
+                <p class="card-text fw-bold text-center">FASILKOM GOES TO SCHOOL</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="card" data-aos="flip-left" data-aos-delay="400" data-aos-duration="500">
+              <img src="img/project/4.jpg" class="card-img-top" alt="Projects 4" />
+              <div class="card-body">
+                <p class="card-text fw-bold text-center">SIREG B INFORMATION SYSTEM</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="card" data-aos="flip-left" data-aos-delay="500" data-aos-duration="500">
+              <img src="img/project/5.jpg" class="card-img-top" alt="Projects 5" />
+              <div class="card-body">
+                <p class="card-text fw-bold text-center">BEM CABINET FAPARATION</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#ffff"
+          fill-opacity="1"
+          d="M0,160L48,138.7C96,117,192,75,288,90.7C384,107,480,181,576,197.3C672,213,768,171,864,181.3C960,192,1056,256,1152,234.7C1248,213,1344,107,1392,53.3L1440,0L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+        ></path>
+      </svg>
+    </section>
+
+    <!-- Akhir Projects -->
+
+    <!-- Contact -->
+    <section id="message">
+      <div class="container">
+        <div class="row text-center mb-3">
+          <div class="col">
+            <h2><p class="fw-light">IMPLIED MESSAGE</p></h2>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-6">
+            <div class="alert alert-warning alert-dismissible fade show d-none my-alert" role="alert">
+              <strong>Terima kasih!</strong> Pesan anda sudah kami terima.
+              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            <form name="WPU-Contact-Form">
+              <div class="mb-3">
+                <label for="name" class="form-label">Nama Lengkap</label>
+                <input type="text" class="form-control" id="name" aria-describedby="name" name="nama" />
+              </div>
+              <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" aria-describedby="email" name="email"/>
+              </div>
+              <div class="mb-3">
+                <label for="Pesan" class="form-label">Pesan</label>
+                <textarea class="form-control" id="pesan" rows="3" name="pesan"></textarea>
+              </div>
+
+              <button type="submit" class="btn btn-dark btn-kirim">Kirim</button>
+
+              <button class="btn btn-dark btn-loading d-none" type="button" disabled>
+                <span class="spinner-border spinner-border-sm role="status aria-hidden="true" ></span>
+                Loading...
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#212529"
+          fill-opacity="1"
+          d="M0,160L60,144C120,128,240,96,360,74.7C480,53,600,43,720,58.7C840,75,960,117,1080,138.7C1200,160,1320,160,1380,160L1440,160L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+        ></path>
+      </svg>
+    </section>
+
+    <!-- Akhir Contact -->
+
+
+    <!-- Footer -->
+    <footer class="bg-dark text-white text-center pb-3">
+      <p class="font-monospace fw-bold fw-light">
+        <i class="bi bi-instagram"></i> juansavero_</a> <i class="bi bi-twitter"></i> saverojuann <i class="bi bi-linkedin"></i> Juan Savero
+        <i class="bi bi-envelope"></i> juansavero27@gmail.com
+      </p>
+        
+    </footer>
+
+    <!-- Akhir Footer -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+ 
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+      AOS.init({
+        once: true,
+        duration: 2000,
+      });
+    </script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.10.4/gsap.min.js"></script>
+    <script>
+      gsap.from('.jumbotron img', {duration: 1,rotateY:360, opacity: 0, ease: 'bounce'});
+      gsap.form('.navbar', {duration: 1.5, y: '-100%', opacity: 0, ease:'bounce'});
+ 
+    </script>
+    <script>
+      const scriptURL = 'https://script.google.com/macros/s/AKfycbxnoLFBZUSyoDY3wpKBcEdZnR2HD8bbYd2o7h2ji_Xb7JFTgkG5Nw4GJNQTvPGaKEIz/exec'
+      const form = document.forms['WPU-Contact-Form']
+      const btnKirim = document.querySelector('.btn-kirim');
+      const btnLoading = document.querySelector('.btn-loading');
+      const myAlert = document.querySelector('.my-alert');
+    
+      form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        //ketika tombol submit diklik
+        //tampilkan tombol loading, hilangan tombol kirim
+        btnLoading.classList.toggle('d-none');
+        btnKirim.classList.toggle('d-none');
+        fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+          .then((response) => {
+            //tampilkan tombol kirim, hilangan tombol loading
+            btnLoading.classList.toggle('d-none');
+            btnKirim.classList.toggle('d-none');
+            // tampilkan alert
+            myAlert.classList.toggle('d-none');
+            // reset formnya
+            form.reset();
+            console.log('Success!', response);
+          })
+          .catch((error) => console.error('Error!', error.message));
+      });
+    </script>
+  </body>
+</html>
